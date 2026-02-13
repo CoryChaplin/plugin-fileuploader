d374ce929414b5bc4aee645c22d8dca95508ef38
Cory Chaplin <cory.chaplin@laposte.net>
2026-02-13 23:25:31 +0100

    Revert "Move X-Forwarded-Proto middleware to global level"
    This reverts commit 5eb446aa6974cc4a3942c24eaa8645b1fafcbed5.

--- server/uploadserver.go
+++ server/uploadserver.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"crypto/tls"
 	"net/http"
 	"sync"
 
@@ -51,16 +50,6 @@ func init() {
 // Run starts the UploadServer
 func (serv *UploadServer) Run(replaceableHandler *ReplaceableHandler) error {
 	serv.Router = gin.New()
-
-	// Handle X-Forwarded-Proto from reverse proxy (must be first middleware)
-	serv.Router.Use(func(c *gin.Context) {
-		if proto := c.Request.Header.Get("X-Forwarded-Proto"); proto == "https" {
-			c.Request.URL.Scheme = "https"
-			c.Request.TLS = &tls.ConnectionState{} // Signal that this is a TLS connection
-		}
-		c.Next()
-	})
-
 	serv.Router.Use(logging.GinLogger(serv.log), gin.Recovery())
 
 	serv.DBConn = db.ConnectToDB(serv.log, db.DBConfig{
